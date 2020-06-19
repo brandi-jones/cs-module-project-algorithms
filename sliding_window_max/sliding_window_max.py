c0e4754ab181f495ff1ee6a0a3b43543bbe57de3
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-06-19 11:06:08 -0400

    mvp day 2

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,22 +2,42 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+# def sliding_window_max(nums, k):
+#     # Your code here
+#     maxNums = []
+#     index, end = 0, len(nums)
+    
+#     while index + k <= end:
+#         maxNums.append(max(nums[index:index + k]))
+#         index+=1
+
+#     return maxNums
+
+from collections import deque
+    
 def sliding_window_max(nums, k):
     # Your code here
     maxNums = []
-    index, end = 0, len(nums)
+    Deque = deque()
+    for i, num in enumerate(nums):
+        while len(Deque) > 0 and num > Deque[-1]:
+            Deque.pop()
+        Deque.append(num)
+        window = i - k + 1
+        if window >= 0:
+            maxNums.append(Deque[0])
+            if nums[window] == Deque[0]:
+                Deque.popleft()
+
     
-    while index + k <= end:
-        maxNums.append(max(nums[index:index + k]))
-        index+=1
 
     return maxNums
     
-
+    
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
-    arr = [1, 3, -1, -3, 5, 3, 6, 7]
+    #arr = [1, 3, -1, -3, 5, 3, 6, 7]
     arr = [1, 2, 5, 7, 5, 9] #answer should be [5, 7, 7, 9]
     k = 3
 
